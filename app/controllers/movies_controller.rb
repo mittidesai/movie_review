66dcfe2d2b6435060b15e51b532755cb9026255d
Jatan Patel <jatan.92@gmail.com>
2016-04-26 22:19:24 +0000

    search

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,11 @@
 class MoviesController < ApplicationController
-    before_action :authenticate_admin!, except: [:show]
+    before_action :authenticate_admin!, except: [:show, :search]
     def new
         @movie = Movie.new
     end
-
+    def search
+        @results = Movie.search(params[:search])
+    end
     def index
         @movies = Movie.all
     end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,12 @@ class Movie < ActiveRecord::Base
     has_many :reviews, dependent: :destroy
     has_attached_file :poster, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :poster, content_type: /\Aimage\/.*\Z/
+    def self.search(search)
+        if search
+            #where(:all, :conditions => ['title LIKE ?', 'genre LIKE ?', 'dname LIKE ?', 'actor1 LIKE ?', 'actor2 LIKE ?'])
+            where(['lower(title) LIKE ? or lower(genre) LIKE ? or lower(dname) LIKE ? or lower(actor1) LIKE ? or lower(actor2) LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%"])
+        #else
+         #   find(:all)
+        end
+    end
 end
