3420038d69dec8b9f89abcd90ff7ee9163208ffa
Jatan Patel <jatan.92@gmail.com>
2016-04-28 01:04:27 +0000

    watchlist

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160427184106) do
+ActiveRecord::Schema.define(version: 20160428001740) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -78,5 +78,15 @@ ActiveRecord::Schema.define(version: 20160427184106) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "watchlists", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "movie_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "watchlists", ["movie_id"], name: "index_watchlists_on_movie_id"
+  add_index "watchlists", ["user_id"], name: "index_watchlists_on_user_id"
 
 end
